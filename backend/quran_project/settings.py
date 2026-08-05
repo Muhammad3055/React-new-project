4c547e237677bd8a136da5d9837c5348bc0ddfd5
Quran Portal Developer <developer@quranportal.org>
2026-08-05 20:36:13 +0500

    Add CSRF_TRUSTED_ORIGINS to settings.py to fix Django Admin HTTPS 500/403 error

--- backend/quran_project/settings.py
+++ backend/quran_project/settings.py
@@ -14,6 +14,14 @@ DEBUG = env.bool('DEBUG', default=True)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://maktabatulmuslim.com',
+    'https://www.maktabatulmuslim.com',
+    'http://localhost:5173',
+    'http://127.0.0.1:8000',
+    'http://localhost:8000'
+]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
